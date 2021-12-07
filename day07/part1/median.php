f3faaa52ef6738cd9ed0f493498957f52ddc4b49
bitlyrist <brad@bitlyrist.com>
2021-12-07 13:07:44 -0500

    median updates

--- day07/part1/median.php
+++ day07/part1/median.php
@@ -11,13 +11,13 @@ $crabs = read_from_stdin();
 sort($crabs);
 $count_crabs = count($crabs);
 $count_crabs = ($count_crabs % 2) == 0 ? $count_crabs - 1 : $count_crabs + 1;
-$floor_median = floor($count_crabs / 2);
-$ceil_median = ceil($count_crabs / 2);
+$floor_crab = $crabs[floor($count_crabs / 2)];
+$ceil_crab = $crabs[ceil($count_crabs / 2)];
 $floor_sum = 0;
 $ceil_sum = 0;
 foreach ($crabs as $crab) {
-  $floor_sum += abs($crab - $crabs[$floor_median]);
-  $ceil_sum += abs($crab - $crabs[$ceil_median]);
+  $floor_sum += abs($crab - $floor_crab);
+  $ceil_sum += abs($crab - $ceil_crab);
 }
 
 printf("Minimum fuel usage is: %d\n", min($floor_sum, $ceil_sum) );
