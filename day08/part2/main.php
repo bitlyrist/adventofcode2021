8ad328f4bcc7482aaa1942cc3746357cce113e8c
bitlyrist <brad@bitlyrist.com>
2021-12-09 08:40:23 -0500

    Day 8 Code cleanup

--- day08/part2/main.php
+++ day08/part2/main.php
@@ -15,50 +15,27 @@ function read_from_stdin(&$sequence, &$digits) {
 // Read all the contents from the line (assuming our alzheimers isn't acting up)
 read_from_stdin($sequence, $digits);
 
-$counts = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 $sum = 0;
 foreach ($sequence as $lidx => $line) {
+  
   $digit_arrangement = array();
-  foreach ($line as $didx => $digit) {
-    $len = strlen($digit);
-    switch ($len) {
-      case 2:
-        $counts[1]++;
-        add_code($digit_arrangement, 1, $digit);
-        break;
-      case 3:
-        $counts[7]++;
-        add_code($digit_arrangement, 7, $digit);
-        break;
-      case 4:
-        $counts[4]++;
-        add_code($digit_arrangement, 4, $digit);
-        break;
-      case 7:
-        $counts[8]++;
-        add_code($digit_arrangement, 8, $digit);
-        break;
-    }
-  }
-
   while ( count($digit_arrangement) < 10) {
-    foreach ($line as $didx => $digit) {
+    foreach ($line as $digit) {
+      $digit = str_split($digit);
       infer_code($digit_arrangement, $digit);
     }
   }
 
   $lookup = array();
   foreach ( $digit_arrangement as $num=>$da) {
-    sort($da);
-    $key = implode('', $da);
-    $lookup[$key] = '' . $num;
+    $lookup[ implode('', $da) ] = '' . $num;
   }
   
   $num_string = '';
   foreach ($digits[$lidx] as $didx => $digit) {
-    $digit_array = str_split($digit);
-    sort($digit_array);
-    $digit_str = implode('', $digit_array);
+    $digit_arr = str_split($digit);
+    sort($digit_arr);
+    $digit_str = implode('', $digit_arr);
     $num_string .= $lookup[$digit_str];
   }
 
@@ -74,34 +51,44 @@ printf("Total sum is: %d\n", $sum );
 
 
 function add_code(&$arrangement, $index, $digits) {
-  $darr = str_split($digits);
-  sort($darr);
-  $arrangement[$index] = $darr;
+  sort($digits);
+  $arrangement[$index] = $digits;
 }
 
 function infer_code(&$arrangement, $digit) {
-  $digit_array = str_split($digit);
-  sort($digit_array);
-  $digit_len = strlen($digit);
-  $digit_arr = str_split($digit);
+  $digit_str = implode('', $digit);
+  $digit_len = strlen($digit_str);
   switch ($digit_len) {
+    case 2:
+      add_code($arrangement, 1, $digit);
+      break;
+    case 3:
+      add_code($arrangement, 7, $digit);
+      break;
+    case 4:
+      add_code($arrangement, 4, $digit);
+      break;
+    case 7:
+      add_code($arrangement, 8, $digit);
+      break;
     case 6:
-      if ( difference($arrangement[4], $digit_arr) == 0) {
-        $arrangement[9] = $digit_array;
-      } else if ( difference($arrangement[1], $digit_arr) == 0 ) {
-        $arrangement[0] = $digit_array;
-      } else if ( difference($arrangement[1], $digit_arr) == 1 ) {
-        $arrangement[6] = $digit_array;
+      if ( array_key_exists(4, $arrangement) && difference($arrangement[4], $digit) == 0) {
+        add_code($arrangement, 9, $digit);
+      } else if ( array_key_exists(1, $arrangement) && difference($arrangement[1], $digit) == 0 ) {
+        add_code($arrangement, 0, $digit);
+      } else if ( array_key_exists(1, $arrangement) && difference($arrangement[1], $digit) == 1 ) {
+        add_code($arrangement, 6, $digit);
       }
       break;
     case 5:
-      if ( difference($arrangement[1], $digit_arr) == 0) {
-        $arrangement[3] = $digit_array;
-      } else if ( difference($arrangement[4], $digit_arr) == 1 ) {
-        $arrangement[5] = $digit_array;
-      } else if ( difference($arrangement[4], $digit_arr) == 2 ) {
-        $arrangement[2] = $digit_array;
-      } 
+      if ( array_key_exists(1, $arrangement) && difference($arrangement[1], $digit) == 0) {
+        add_code($arrangement, 3, $digit);
+      } else if ( array_key_exists(4, $arrangement) && difference($arrangement[4], $digit) == 1 ) {
+        add_code($arrangement, 5, $digit);
+      } else if ( array_key_exists(4, $arrangement) && difference($arrangement[4], $digit) == 2 ) {
+        add_code($arrangement, 2, $digit);
+      }
+      break;
   }
 }
 
